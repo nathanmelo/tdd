b2922891875f04461e3ab2bac844831a18ba17c1
Nathan Melo <nathan.vix@gmail.com>
2015-12-21 22:20:34 -0200

    Refatorada a classe Category

--- 4-custom_matchers/spec/category_spec.rb
+++ 4-custom_matchers/spec/category_spec.rb
@@ -4,23 +4,6 @@ require "subcategory"
 require "custon"
 
 
-RSpec::Matchers.define :contain_products do |*products|
-	match do |category|
-		subcategories_products = category.subcategories.map { |sub|
-			sub.products
-		}
-		subcategories_products.flatten!
-		
-		expect(subcategories_products & products).to eq products
-		#expect(subcategories_products & products).to eq products
-	end
-
-	failure_message do |category|
-		"expect category #{category.name} to contain products #{products}"
-	end
-end
-
-
 describe Category do
 	it "contains all the products of its subcategories" do
 		#criadas categorias
