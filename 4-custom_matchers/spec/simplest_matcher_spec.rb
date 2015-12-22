3cc618d5bf8bb2378433359ef31bcd089e3c581b
Nathan Melo <nathan.vix@gmail.com>
2015-12-21 22:52:35 -0200

    Criação de hook (before)

--- 4-custom_matchers/spec/simplest_matcher_spec.rb
+++ 4-custom_matchers/spec/simplest_matcher_spec.rb
@@ -3,7 +3,11 @@ require "spec_helper"
 describe "The matcher protocol" do 
 	context "a minimal matcher" do
 		it "has a #matches?(actual) method" do
-			expect("anything").to SimplestMatcher.new
+			expect("anything").to simple_matcher
+		end
+
+		def simple_matcher
+			SimplestMatcher.new
 		end
 	end
 	
---
+++ 5-rspec_hooks/lib/game.rb
@@ -0,0 +1,9 @@
+describe Game, "in the final phase" do
+	context "when the player hits the target" do
+
+		it "congratulates the player"
+
+		it "sets the score to 100"
+		
+	end
+end
