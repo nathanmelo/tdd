abf76f08247dd5fc74616013353b85b605ad07a6
Nathan Melo <nathan.vix@gmail.com>
2015-12-27 22:57:57 -0200

    Criação de testes para verificção de pontuação do game (antes da implementação do DRY)

--- 5-rspec_hooks/spec/game.rb
+++ 5-rspec_hooks/spec/game.rb
@@ -1,17 +1,48 @@
 #require "spec_helper"
 class Game
 
-	def initialize
-		@game = Game.new
+	attr_reader :output, :phase, :score
+
+	def player_hits_target
+		@output = "Congratulations !"
+		@score = 100
+	end
+
+	def phase=(phase)
+		@phase = phase 
+	end
+
+	def score 
+		@score
 	end
 
 end
 describe Game , "in the final phase" do
 	context "when the player hits the target" do
 
-		it "congratulates the player"
+		it "congratulates the player" do
+			game = Game.new
+			game.phase = :final
 
-		it "sets the score to 100"
+			game.player_hits_target
+			puts "***************************************"
+			puts game.output
+			puts "***************************************"
+			expect(game.output).to eq("Congratulations !")
+		end
+
+		it "sets the score to 100" do
+			game = Game.new
+			game.phase = :final
+
+			game.player_hits_target
+
+			puts "***************************************"
+			puts game.score
+			puts "***************************************"
+
+			expect(game.score).to eq(100)
+		end
 
 	end
 end
