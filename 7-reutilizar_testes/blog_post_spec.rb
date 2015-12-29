bae823eaa38a89fe6ee504f2a90f3b8b705c72ee
Nathan Melo <nathan.vix@gmail.com>
2015-12-29 21:08:20 -0200

    Criação de classe para teste de posts em blog

--- 7-reutilizar_testes/blog_post_spec.rb
+++ 7-reutilizar_testes/blog_post_spec.rb
@@ -1,3 +1,12 @@
+class BlogPost
+	attr_reader :published_on
+
+	def publish!
+		today = Time.now.strftime("%Y-%m-%d")
+		@published_on = today
+	end
+end
+
 describe BlogPost do 
 	describe "#publish!" do
 		it "saves the publication date" do
